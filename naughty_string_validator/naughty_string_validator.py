92e0eeaf7353eb3df143c6186bba9f4e4222e4b3
Shashi Raja <sraja@Ban-1Sraja-m.local>
2018-08-31 23:17:40 +0530

    added emoji db

--- naughty_string_validator/naughty_string_validator.py
+++ naughty_string_validator/naughty_string_validator.py
@@ -4,7 +4,13 @@ import os
 # print os.listdir(r'naughty_string_db')
 with open(r'naughty_string_db/blns.json', 'r') as f:
     naughty_string_list = json.loads(f.read())
+with open(r'naughty_string_db/emoji.json', 'r') as f:
+    emoji_list = json.loads(f.read())
 
 def get_naughty_string_list():
     """ Get entire naughty string list """
     return naughty_string_list
+
+def get_emoji_list():
+    """ Get entire naughty string list """
+    return emoji_list
