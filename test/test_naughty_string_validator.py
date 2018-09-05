f9d9b27d11ecb1a07ba6d4a4a33ede3802118b55
Shashi Kumar Raja <shashiraja92@gmail.com>
2018-09-05 12:15:55 +0530

    Revert "Added class for NaughtyStringValidator"

--- test/test_naughty_string_validator.py
+++ test/test_naughty_string_validator.py
@@ -3,49 +3,43 @@ Tests for `naughty_string_validator` module.
 """
 from __future__ import unicode_literals
 
-import inspect
+import sys
 import os
+import pytest
 from builtins import str
 
-from naughty_string_validator import NaughtyStringValidator, SRC_DIRECTORY
+from naughty_string_validator import *
 
-
-class TestNaughtyStringValidator(object):
+class TestNaughty_string_validator(object):
 
     @classmethod
     def setup_class(cls):
-        cls.string_validator = NaughtyStringValidator()
         pass
 
     def test_presence_of_db_files(self):
-        assert len(os.listdir(os.path.join(SRC_DIRECTORY, 'naughty_string_db'))) == 2
+        assert len(os.listdir('naughty_string_validator/naughty_string_db')) == 2
 
     def test_function_names_of_naughty_string_validator(self):
-        validator_methods = [
-            attr for attr in dir(self.string_validator) if inspect.ismethod(getattr(self.string_validator, attr))
-        ]
-        assert 'get_naughty_string_list' in validator_methods
-        assert 'get_emoji_list' in validator_methods
-        assert 'get_random_naughty_string' in validator_methods
-        assert 'get_random_emoji' in validator_methods
+        main_module = naughty_string_validator.__all__
+        assert 'get_naughty_string_list' in main_module and 'get_emoji_list' in main_module and 'get_random_naughty_string' in main_module and 'get_random_emoji' in main_module
 
     def test_len_of_naughty_string_list(self):
-        assert len(self.string_validator.get_naughty_string_list()) > 0
-
+        assert len(get_naughty_string_list()) > 0
+    
     def test_type_of_naughty_string_list(self):
-        assert type(self.string_validator.get_naughty_string_list()) is list
+        assert type(get_naughty_string_list()) is list
 
     def test_len_of_emoji_list(self):
-        assert len(self.string_validator.get_emoji_list()) > 0
+        assert len(get_emoji_list()) > 0
 
     def test_type_of_emoji_list(self):
-        assert type(self.string_validator.get_emoji_list()) is list
+        assert type(get_emoji_list()) is list
 
     def test_type_of_random_naughty_string(self):
-        assert isinstance(self.string_validator.get_random_naughty_string(), str)
+        assert isinstance(get_random_naughty_string(), str) == True
 
     def test_type_of_random_emoji(self):
-        assert isinstance(self.string_validator.get_random_emoji(), str)
+        assert isinstance(get_random_emoji(), str) == True
 
     @classmethod
     def teardown_class(cls):
